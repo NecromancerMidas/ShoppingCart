105c0670f8051650fb1b3fd418d7409e610b510f
NecromancerMidas <115973453+NecromancerMidas@users.noreply.github.com>
2023-04-14 18:18:25 +0200

    x

--- ShoppingCart/Program.cs
+++ ShoppingCart/Program.cs
@@ -7,17 +7,7 @@ namespace ShoppingCart
     {
         public static void Main(string[] args)
         {
-            var shoppingCart = new ShoppingCart();
-            shoppingCart.Show();
-            var productA = new Product("A", 40);
-            var productB = new Product("B", 70);
-            var productC = new Product("C", 30);
-            shoppingCart.Buy(productA,1);
-            shoppingCart.Show();
-            shoppingCart.Buy(productB,3);
-            shoppingCart.Show();
-            shoppingCart.Buy(productA,5);
-            shoppingCart.Show();
+            SimpleShoppingCart2.Run();
         }
     }
 }
---
+++ ShoppingCart/SimpleShoppingCart2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart
+{
+    internal class SimpleShoppingCart2
+    {
+        public static void Run()
+        {
+            var shoppingCart = new ShoppingCart();
+            shoppingCart.Show();
+            var productA = new Product("A", 40);
+            var productB = new Product("B", 70);
+            var productC = new Product("C", 30);
+            shoppingCart.Buy(productA, 1);
+            shoppingCart.Show();
+            shoppingCart.Buy(productB, 3);
+            shoppingCart.Show();
+            shoppingCart.Buy(productA, 5);
+            shoppingCart.Show();
+        }
+    }
+}
